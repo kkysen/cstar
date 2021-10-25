f0dfa4d71e0c83b84b1aefa83948ebb66bb27e1e
Khyber Sen <kkysen@gmail.com>
2021-10-25 03:56:07 -0400

    Added the lexer token definition.

---
+++ src/token.mli
@@ -0,0 +1,71 @@
+type comment =
+  | Structural (* /- ... *)
+  | Line (* // *) of string
+  | Block (* /* */ *) of string
+
+type number_base =
+  | Binary (* 0b *)
+  | Octal (* 0o *)
+  | HexaDecimal (* 0x *)
+  | Decimal
+
+type number_base =
+  | Binary (* 0b *)
+  | Octal (* 0o *)
+  | HexaDecimal (* 0x *)
+  | Decimal
+
+type number_literal = {
+    base : number_base
+  ; number : string
+  ; suffix : string (* i32, f64, u1, x1000, usize, "" *)
+}
+
+type char_literal = {
+    unescaped : string;
+    prefix : string;
+}
+
+type string_literal = {
+    unescaped : string
+  ; prefix : string
+}
+
+type literal =
+  | Number of number_literal
+  | Char of char_literal
+  | String of string_literal
+
+type token =
+  | WhiteSpace (* ' \n\r\t', ... *)
+  | Comment of comment
+  | Literal of literal
+  | Identifier of string
+  | SemiColon (* ; *)
+  | Colon (* : *)
+  | Comma (* , *)
+  | Dot (* . *)
+  | OpenParen (* ( *)
+  | CloseParen (* ) *)
+  | OpenBrace (* { *)
+  | CloseBrace (* } *)
+  | OpenBracket (* [ *)
+  | CloseBracket (* ] *)
+  | At (* @ *)
+  | QuestionMark (* ? *)
+  | ExclamationPoint (* ! *)
+  | Equal (* = *)
+  | LessThan (* < *)
+  | GreaterThan (* > *)
+  | Plus (* + *)
+  | Minus (* - *)
+  | Times (* * *)
+  | Divide (* / *)
+  | And (* & *)
+  | Or (* | *)
+  | Caret (* ^ *)
+  | Percent (* % *)
+  | Tilde (* ~ *)
+  | Pound (* # *)
+  | DollarSign (* $ *)
+  | Unknown
