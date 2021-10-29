52486600d7af1f4a98d840bf0fdd240ed3519d0d
Khyber Sen <kkysen@gmail.com>
2021-10-29 02:08:53 -0400

    Added `deriving show`s to the tokens.

--- src/token.mli
+++ src/token.mli
@@ -2,40 +2,48 @@ type comment =
   | Structural (* /- ... *)
   | Line (* // *) of string
   | Block (* /* */ *) of string
+[@@deriving show]
 
 type number_base =
   | Binary (* 0b *)
   | Octal (* 0o *)
   | HexaDecimal (* 0x *)
   | Decimal
+[@@deriving show]
 
 type sign =
   | None
   | Positive
   | Negative
+[@@deriving show]
 
 type raw_int_literal = {
     sign : sign
   ; base : number_base
   ; number : string
 }
+[@@deriving show]
 
 type number_literal = {
     integral : raw_int_literal
-  ; floating : raw_int_literal option (* is a float if it has a floating part *)
-  ; exponent : raw_int_literal option
+        (*; floating : raw_int_literal option*)
+        (* is a float if it has a floating part *)
+        (* ; exponent : raw_int_literal option *)
   ; suffix : string (* i32, f64, u1, x1000, usize, "" *)
 }
+[@@deriving show]
 
 type char_literal = {
     unescaped : string
   ; prefix : string
 }
+[@@deriving show]
 
 type string_literal = {
     unescaped : string
   ; prefix : string
 }
+[@@deriving show]
 
 (* TODO format_string_literal *)
 
@@ -43,8 +51,10 @@ type literal =
   | Number of number_literal
   | Char of char_literal
   | String of string_literal
+[@@deriving show]
 
 type token =
+  | EOF
   | WhiteSpace (* ' \n\r\t', ... *)
   | Comment of comment
   | Literal of literal
@@ -77,3 +87,4 @@ type token =
   | Pound (* # *)
   | DollarSign (* $ *)
   | Unknown
+[@@deriving show]
