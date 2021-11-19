3dd4f546346a4c90068ec6f93f017b36bb13b293
Khyber Sen <kkysen@gmail.com>
2021-11-19 14:56:05 -0500

    Improved setup script and made it work on macOS.

--- setup.sh
+++ setup.sh
@@ -17,30 +17,86 @@ is-command() {
 link() {
     local from="${1}"
     local to="${2}"
+    # macos `ln` doesn't support full flag names like `--symbolic --force`
     ln -s -f "${from}" "${bin_dir}/${to}"
 }
 
+link-on-path() {
+    local exe_name="${1}"
+    [[ -x "${bin_dir}/${exe_name}" ]] && return
+    link "$(which "${exe_name}")" "${exe_name}"
+}
+
+cached-install() {
+    local install_func="${1}"
+    local package_name="${2}"
+    local exe_name="${3:-}"
+    local exe_dir="${exe_dir:-}"
+    local which_prefix="${which_prefix:-}"
+    if [[ "${exe_name}" == "" ]]; then
+        exe_name="${package_name}"
+    fi
+    is-command "${exe_name}" && return
+    install_func "${package_name}" "${exe_name}"
+    if [[ "${exe_dir:-}" == "" ]]; then
+        local exe_path=$("${which_prefix}" which "${exe_name}")
+    else
+        local exe_path="${exe_dir}/${exe_name}"
+    fi
+    link "${exe_path}" "${exe_name}"
+}
+
+if is-command brew; then
+    link-on-path brew
+    package_installer=("brew")
+elif is-command apt; then
+    link-on-path apt
+    package_installer=("sudo" "apt")
+else
+    echo >&2 "can't find `brew` or `apt`"
+    exit 1
+fi
+
+package-install-raw() {
+    local package_name="${1}"
+    "${package_installer[@]}" "${1}"
+}
+
+package-install() {
+    cached-install package-install "${@}"
+}
+
 install-ccache() {
-    is-command ccache || sudo apt install ccache
+    is-command ccache || install-package ccache
+    [[ -f "${bin_dir}/ccache" ]] || link-on-path ccache
     ccache --max-files 0 --max-size 0
-    [[ -f "${bin_dir}/ccache" ]] || link "$(which ccache)" ccache
     for compiler in cc c++ gcc g++ clang clang++; do
-        link ./ccache "./${bin_dir}/${compiler}"
+        link ./ccache "${compiler}"
     done
 }
 
-install-opam() {
-    is-command opam && return
+install-opam-raw() {
+    install-package curl
     sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)
     eval "$(opam env)"
 }
 
+install-opam() {
+    is-command opam || install-opam-raw
+    link-on-path opam
+}
+
 cargo_bin="${CARGO_HOME:-${HOME}/.cargo}/bin"
 
-install-cargo() {
-    is-command cargo && return
+install-cargo-raw() {
+    install-package curl
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     export PATH="${cargo_bin}:${PATH}"
+}
+
+install-cargo() {
+    is-command cargo || install-cargo-raw
+    link-on-path cargo
 }
 
 cargo-has() {
@@ -48,17 +104,17 @@ cargo-has() {
     [[ -f "${cargo_bin}/${exe_name}" ]]
 }
 
-cargo-install() {
+cargo-install-raw() {
     local package_name="${1}"
-    local exe_name="${2:-}"
-    if [[ "${exe_name}" == "" ]]; then
-        exe_name="${package_name}"
-    fi
+    local exe_name="${2}"
     cargo-has "${exe_name}" || (cargo-has cargo-quickinstall \
         && cargo quickinstall "${package_name}" \
         || cargo install "${package_name}" \
     )
-    link "${cargo_bin}/${exe_name}" "${exe_name}"
+}
+
+cargo-install() {
+    exe_dir="${cargo_bin}" cached-install cargo-install-raw "${@}"
 }
 
 install-from-opam() {
@@ -80,31 +136,52 @@ install-from-cargo() {
     cargo-install hyperfine
 }
 
-install-from-vscode() {
-    is-command code || return
-    code --install-extension ocamllabs.ocaml-platform
+vscode-extension-install() {
+    local name="${1}"
+    local num_found=$(fd \
+        -uu \
+        --type directory \
+        --exact-depth 1 \
+        --fixed-strings "${name}" \
+        ~/.vscode-server/extensions/ \
+        | wc -l)
+    if [[ ${num_found} -gt 0 ]]; then
+        return
+    fi
+    code --install-extension "${name}"
 }
 
-install-volta() {
-    is-command volta && return
+install-from-vscode() {
+    is-command code || return
+    link-on-path code
+    vscode-extension-install ocamllabs.ocaml-platform
+    vscode-extension-install skellock.just
+}
+
+install-volta-raw() {
+    install-package curl
     curl https://get.volta.sh | bash
     export VOLTA_HOME="${HOME}/.volta"
     export PATH="${VOLTA_HOME}/bin:${PATH}"
 }
 
-npm-install() {
+install-volta() {
+    is-command volta || install-volta-raw
+    link-on-path volta
+}
+
+npm-install-raw() {
     local package_name="${1}"
-    local exe_name="${2:-}"
-    if [[ "${exe_name}" == "" ]]; then
-        exe_name="${package_name}"
-    fi
-    is-command "${exe_name}" || volta install "${package_name}"
-    link "$(volta which "${exe_name}")" "${exe_name}"
+    volta install "${package_name}"
+}
+
+npm-install() {
+    which_prefix=volta cached-install npm-install-raw "${@}"
 }
 
 install-from-npm() {
     install-volta
-    volta install node@latest
+    npm-install node@latest node
     npm-install esy
 }
 
@@ -115,33 +192,68 @@ install-llvm() {
             return
         fi
     fi
-    sudo apt install -y lsb-release wget software-properties-common || true
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    sudo ./llvm.sh "${llvm_version}"
-    rm llvm.sh
-    link "$(llvm-config-${llvm_version} --bindir)" "llvm"
+    case "${OSTYPE}" in
+        linux*)
+            sudo apt install -y lsb-release wget software-properties-common
+            wget https://apt.llvm.org/llvm.sh
+            chmod +x llvm.sh
+            sudo ./llvm.sh "${llvm_version}"
+            rm llvm.sh
+            local llvm_config_suffix="-${llvm_version}"
+            ;;
+        darwin*)
+            brew install "llvm@${llvm_version}" || brew reinstall llvm
+            local llvm_config_suffix=""
+            ;;
+        *)
+            echo >&2 "unsupported platform: ${OSTYPE}"
+            return 1
+            ;;
+    esac
+    link "$(llvm-config${llvm_config_suffix} --bindir)" "llvm"
+}
+
+install-in-parallel() {
+    local pids=()
+    for func in "${@}"; do
+        "${func}" &
+        pids+=($!)
+    done
+    local i=0
+    for pid in ${pids[@]}; do
+        wait ${pid}
+        local status=$?
+        i=$((i + 1))
+        if [[ ${status} -ne 0 ]]; then
+            echo kill ${pids[@]:i}
+            kill ${pids[@]:i}
+            return ${status}
+        fi
+    done
+}
+
+install-build-deps() {
+    install-ccache
+    install-in-parallel install-from-opam install-from-npm install-from-cargo
 }
 
 install-build() {
-    install-ccache &
-    install-from-opam &
-    install-from-npm &
-    install-from-cargo &
-    install-llvm &
-    wait
+    install-build-deps
     esy
 }
 
 install-dev-only() {
-    install-from-vscode &
-    wait
+    install-in-parallel install-from-vscode
+}
+
+install-dev-deps() {
+    install-build-deps
+    install-from-vscode
 }
 
 install-dev() {
-    install-build &
-    install-dev-only &
-    wait
+    install-dev-deps
+    esy
     just build
 }
 
