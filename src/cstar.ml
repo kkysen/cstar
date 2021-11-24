366826abc198ed8a33f0ec9d63a27e388c892289
Khyber Sen <kkysen@gmail.com>
2021-11-24 06:09:11 -0500

    Changed the hardcoded hello world to use the llvm ocaml bindings instead of hardcoding the llvm ir string.

--- src/cstar.ml
+++ src/cstar.ml
@@ -184,23 +184,43 @@ let range_inclusive ~(min : int) ~(max : int) : int list =
   range ~min ~max:(max + 1)
 ;;
 
+module LL = Llvm
+module LLAnalysis = Llvm_analysis
+module LLTarget = Llvm_target
+
 let compile_cstar ~(src : string) ~(path : string) : string =
   ignore src;
-  [
-    "; ModuleID = '" ^ path ^ "'"
-  ; "source_filename = \"" ^ path ^ "\""
-  ; "target triple = \"x86_64-pc-linux-gnu\""
-  ; "@.str = constant [14 x i8] c\"Hello, World!\\00\""
-  ; "define i32 @main() {"
-  ; "  %1 = alloca i32, align 4"
-  ; "  store i32 0, i32* %1, align 4"
-  ; "  %2 = call i32 @puts(i8* getelementptr inbounds ([14 x i8], [14 x i8]* \
-     @.str, i64 0, i64 0))"
-  ; "  ret i32 0"
-  ; "}"
-  ; "declare i32 @puts(i8*)"
-  ]
-  |> String.concat ?sep:(Some "\n")
+  LL.enable_pretty_stacktrace ();
+  let ctx = LL.global_context () in
+  let mod_ = LL.create_module ctx path in
+  let target_triple = LLTarget.Target.default_triple () in
+  LL.set_target_triple target_triple mod_;
+  let i8 = LL.i8_type ctx in
+  let i32 = LL.i32_type ctx in
+  let i64 = LL.i64_type ctx in
+  let puts =
+    let type_ = LL.function_type i32 [|LL.pointer_type i8|] in
+    LL.declare_function "puts" type_ mod_
+  in
+  let main =
+    let type_ = LL.function_type i32 [||] in
+    let func = LL.define_function "main" type_ mod_ in
+    let entry = LL.entry_block func in
+    let irb = LL.builder_at_end ctx entry in
+    let hello_world_const = LL.const_stringz ctx "Hello, World!" in
+    let hello_world_global = LL.define_global "" hello_world_const mod_ in
+    let zero_i64 = LL.const_int i64 0 in
+    let hello_world_local =
+      LL.build_in_bounds_gep hello_world_global [|zero_i64; zero_i64|] "" irb
+    in
+    let (_ : LL.llvalue) = LL.build_call puts [|hello_world_local|] "" irb in
+    let zero_i32 = LL.const_int i32 0 in
+    let (_ : LL.llvalue) = LL.build_ret zero_i32 irb in
+    func
+  in
+  LLAnalysis.assert_valid_function main;
+  LLAnalysis.assert_valid_module mod_;
+  LL.string_of_llmodule mod_
 ;;
 
 type emit_type =
