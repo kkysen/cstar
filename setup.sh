20552b03a7513e5313f7e8338c2d88b71e32420d
Khyber Sen <kkysen@gmail.com>
2021-12-03 13:52:44 -0500

    Fixed build-essential install.

--- setup.sh
+++ setup.sh
@@ -241,7 +241,7 @@ install-in-parallel() {
 }
 
 install-build-deps() {
-    package-install build-essential
+    package-install build-essential ""
     install-ccache
     install-from-opam
     install-from-npm
